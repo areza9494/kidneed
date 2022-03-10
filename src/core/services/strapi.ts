7f53a036e84fb7fa87490f915d71f7152dca71ba
Ali Shabani <alishabani9270@gmail.com>
2022-03-10 17:37:16 +0330

    fix: added default api address

--- src/core/services/strapi.ts
+++ src/core/services/strapi.ts
@@ -1,7 +1,7 @@
 import Strapi from "strapi-sdk-js";
 
 export const strapi = new Strapi({
-  url: process.env.NEXT_PUBLIC_STRAPI_URL,
+  url: process.env.NEXT_PUBLIC_STRAPI_URL || "https://api.yekodo.net",
   store: {
     key: "access_token",
     useLocalStorage: true,
